3b660f3d0048df5e05e7a5031808fbc455b34901
Pricon <598384112@qq.com>
2023-04-02 14:17:02 +0800

    update vue.config.js and url

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: "http://localhost:3002",
+    proxy: "http://47.99.121.239:3002",
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/pc-admin/'
